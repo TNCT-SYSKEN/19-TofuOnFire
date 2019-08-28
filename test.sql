53066e9a8a49fc75b4e003f379c30afcbcd04d98
kobanoir <koba20000212@gmail.com>
2019-08-28 14:59:33 +0900

    コマンドまとめた

--- test.sql
+++ test.sql
@@ -27,3 +27,31 @@ CREATE TABLE Nice (
   FOREIGN KEY(PostID) REFERENCES Post(PostID)
 );
 
+-- 各機能で使用する操作(これを使うかどうかは知らない)
+
+/* 一番新しい投稿のIDを取得
+select PostID from post where PostID=(select MAX(PostID) from post); */
+
+/* 投稿リストを取得
+select PostID,UserID,Title,ImageLink1 from post where PostID = x;
+(配列を作成したらこれをループすることでデータを取得？) */
+
+/* 詳細を取得
+select * from post where PostID = x; */
+
+/* プレゼンを投稿する
+insert into post (UserID,Title,ImageLink1,ImageLink2,ImageLink3,ImageLink4) values (...); */
+
+/* 投稿が存在するか確認する
+select * from post where PostID = x; 
+なければNULLなので */
+
+/* 投稿へいいねする
+insert into nice (UserID , PostID) values (...); */
+
+/* 投稿へのいいねを取り消す
+delete from nice where UserID = x and PostID = y; */
+
+/* ユーザが存在するかを取得する
+select * from user where UserID = x; 
+NULLか否かで判断できる */
