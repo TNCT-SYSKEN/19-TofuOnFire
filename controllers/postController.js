946a138bcd5d669269846bd3edabe6f164cd7ce0
arata <arata04962@gmail.com>
2019-08-30 02:09:58 +0900

    [add] 画像のアップロードを仮実装した

--- controllers/postController.js
+++ controllers/postController.js
@@ -9,7 +9,7 @@ controller.postDetail = (req, res, next) => {
 };
 
 controller.postCreate = (req, res, next) => {
-    res.status(501).end();
+    res.status(200).end();
 };
 
 module.exports = controller;
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -5,13 +5,25 @@ const Post = require('../models/post');
 const User = require('../models/user');
 const createError = require('http-errors');
 
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/images/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+    
+const upload = multer({ dest: './public/images', storage: storage });
+
 const postController = require('../controllers/postController');
 
 // 投稿関係のAPI
 
 router.get('/', postController.postList);
 router.get('/:postId(\\d+)', postController.postDetail);
-router.post('/', postController.postCreate);
+router.post('/', upload.array('images', 4), postController.postCreate);
 
 router.param(['postId'], (req, res, next, val) => {
     if (!Post.exists(val)) {
